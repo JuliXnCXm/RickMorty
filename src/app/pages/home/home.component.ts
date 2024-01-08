2e3c918658e9b6b9f94b06b95caf671c317ed68c
julian garcia <juliancamilogarciaescobar7@gmail.com>
2024-01-07 23:41:50 -0500

    character list styled

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { InputSearcherComponent } from '../../components/input-searcher/input-searcher.component';
 import { CharacterListComponent } from '../../components/character-list/character-list.component';
 import { CommonModule } from '@angular/common';
+import { FiltersComponent } from '../../components/filters/filters.component';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavComponent,InputSearcherComponent,CharacterListComponent],
+  imports: [CommonModule, NavComponent,InputSearcherComponent,CharacterListComponent,FiltersComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  currentUrl: string;
   
+  constructor(private location: Location) {
+    this.currentUrl = '';
+  }
+  
+  ngOnInit(): void {
+    this.currentUrl = this.location.path().substring(1);
+  }
 }
